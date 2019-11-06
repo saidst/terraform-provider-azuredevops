3516c351be69eac6c300dca62f85cabf4d6fc1bd
Charles Zipp <chzipp@microsoft.com>
2019-11-06 15:39:16 -0800

    checking in for sharing

--- azuredevops/resource_policy_min_reviewers.go
+++ azuredevops/resource_policy_min_reviewers.go
@@ -1,98 +1,99 @@
 package azuredevops
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	
-	// "github.com/microsoft/azure-devops-go-api/azuredevops/policy"
+	"log"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	// "github.com/microsoft/azure-devops-go-api/azuredevops/policy"
 	// "github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/converter"
 )
 
-
 func resourcePolicyMinReviewers() *schema.Resource {
 	return &schema.Resource{
-	  Create: resourcePolicyMinReviewersCreate,
-	  Read:   resourcePolicyMinReviewersRead,
-	  Update: resourcePolicyMinReviewersUpdate,
-	  Delete: resourcePolicyMinReviewersDelete,
-  
-	  Schema: map[string]*schema.Schema{
-			"project_id" :&schema.Schema{
-			  Type:     schema.TypeString,
-			  Required: true,
+		Create: resourcePolicyMinReviewersCreate,
+		Read:   resourcePolicyMinReviewersRead,
+		Update: resourcePolicyMinReviewersUpdate,
+		Delete: resourcePolicyMinReviewersDelete,
+
+		Schema: map[string]*schema.Schema{
+			"project_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"enabled": &schema.Schema{
-			  Type:     schema.TypeBool,
-			  Optional: true,
-			  Default:	true,
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
 			},
 			"is_blocking": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
-				Default:	true,
+				Default:  true,
 			},
 			"creator_vote_counts": &schema.Schema{
-			  Type:     schema.TypeBool,
-			  Optional: true,
-			  Default:	false,
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
 			},
 			"minimum_approver_count": &schema.Schema{
-			  Type:     schema.TypeInt,
-			  Optional: true,
-			  Default:	1,
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  1,
 			},
 			"scope": {
 				Type:     schema.TypeSet,
 				Required: true,
-				Elem: &schema.Resource {
-				  Schema: map[string]*schema.Schema{
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
 						"repository_id": {
-					  	Type:     schema.TypeString,
-					  	Required: true,
-						},		  
-						"repository_ref": {
-					  	Type:     schema.TypeString,
-					  	Optional:		true,
-					  	Default:		"refs/heads/master",
-						},		  
-						"match_type": {
-					  	Type:      schema.TypeString,
-					  	Optional:		true,
-					  	Default:		"Exact",
+							Type:     schema.TypeString,
+							Required: true,
 						},
-				  },
+						"repository_ref": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "refs/heads/master",
+						},
+						"match_type": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Default:  "Exact",
+						},
+					},
 				},
 			},
 		},
 	}
- }
+}
 
-	func resourcePolicyMinReviewersCreate(d *schema.ResourceData, m interface{}) error {
-		// clients := m.(*aggregatedClient)
-		// projectID := d.Get("project_id").(string)
-		// policyTypes, err := clients.PolicyClient.GetPolicyTypes(clients.ctx, policy.GetPolicyTypesArgs{
-		// 	Project: converter.String(projectID),
-		// })
-		// if err != nil {
-		// 	log.Printf("something bad happened %s\n", err)
-		// }
-		// if (policyTypes != nil){
-		// 	log.Printf("resource_policy_min_reviewers get policy types %+v\n", policyTypes)
-		// }
-		
-		policy_id := "1234" //this should come from api
-		d.SetId(policy_id)
-		return resourcePolicyMinReviewersRead(d, m)
-	}
-	
-	func resourcePolicyMinReviewersRead(d *schema.ResourceData, m interface{}) error {
-		return nil
-	}
+func resourcePolicyMinReviewersCreate(d *schema.ResourceData, m interface{}) error {
+	// clients := m.(*aggregatedClient)
+	// projectID := d.Get("project_id").(string)
+	// policyTypes, err := clients.PolicyClient.GetPolicyTypes(clients.ctx, policy.GetPolicyTypesArgs{
+	// 	Project: converter.String(projectID),
+	// })
+	// if err != nil {
+	// 	log.Printf("something bad happened %s\n", err)
+	// }
+	// if (policyTypes != nil){
+	// 	log.Printf("resource_policy_min_reviewers get policy types %+v\n", policyTypes)
+	// }
 
-	func resourcePolicyMinReviewersUpdate(d *schema.ResourceData, m interface{}) error {
-		return resourcePolicyMinReviewersRead(d, m)
-	}
-	
-	func resourcePolicyMinReviewersDelete(d *schema.ResourceData, m interface{}) error {
-		return nil
-	}
+	policy_id := "1234" //this should come from api
+	projectID := d.Get("project_id").(string)
+	log.Printf("projectID is %s\n", projectID)
+	d.SetId(policy_id)
+	return resourcePolicyMinReviewersRead(d, m)
+}
+
+func resourcePolicyMinReviewersRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourcePolicyMinReviewersUpdate(d *schema.ResourceData, m interface{}) error {
+	return resourcePolicyMinReviewersRead(d, m)
+}
+
+func resourcePolicyMinReviewersDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
--- azuredevops/resource_policy_min_reviewers_test.go
+++ azuredevops/resource_policy_min_reviewers_test.go
@@ -2,12 +2,9 @@ package azuredevops
 
 import (
 	"log"
-	"context"
+	"os"
 	"testing"
 
-	"github.com/microsoft/terraform-provider-azuredevops/azdosdkmocks"
-
-	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	// "github.com/microsoft/azure-devops-go-api/azuredevops/core"
 	// "github.com/microsoft/azure-devops-go-api/azuredevops/operations"
@@ -15,22 +12,14 @@ import (
 )
 
 func TestAzureDevOpsProject_CreatePolicy_DoesStuff(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish();
-	
-	policyClient := azdosdkmocks.NewMockPolicyClient(ctrl)
-	
-	clients := &aggregatedClient{
-		PolicyClient: policyClient,
-		ctx:        context.Background(),
-	}
-
+	clients, err := getAzdoClient(os.Getenv("AZDO_PERSONAL_ACCESS_TOKEN"), os.Getenv("AZDO_ORG_SERVICE_URL"))
 	resource := resourcePolicyMinReviewers()
 	config := map[string]interface{}{
 		"project_id": "foo",
 	}
+
 	resourceData := schema.TestResourceDataRaw(t, resource.Schema, config)
 	err := resourcePolicyMinReviewersCreate(resourceData, clients)
 
-	log.Printf("logging foo %s\n", err);
+	log.Printf("logging foo %s\n", err)
 }
